13675e83e3313f25493b071affa688312701c500
lvminh97 <lvminh97@gmail.com>
2022-11-03 18:30:03 +0700

    Update the UI of Main Activity: - Add Bottom Navigation menu
    Signed-off-by: lvminh97 <lvminh97@gmail.com>

--- app/src/main/java/com/example/smartlight/LoginActivity.java
+++ app/src/main/java/com/example/smartlight/LoginActivity.java
@@ -1,14 +1,49 @@
 package com.example.smartlight;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText usernameEd, passwordEd;
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //
+        initUI();
+    }
+
+    private void initUI() {
+        usernameEd = (EditText) findViewById(R.id.ed_username);
+        passwordEd = (EditText) findViewById(R.id.ed_password);
+
+        loginBtn = (Button) findViewById(R.id.btn_login);
+        loginBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btn_login) {
+            login();
+        }
+    }
+
+    private void login() {
+        if(usernameEd.getText().toString().equals("a") && passwordEd.getText().toString().equals("1")){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(getBaseContext(), "Wrong username or password.\nTry again.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/smartlight/MainActivity.java
+++ app/src/main/java/com/example/smartlight/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.smartlight;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //
+        initUI();
+    }
+
+    private void initUI() {
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.menu_bottom);
+        bottomNavigationView.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                Log.d("MinhLV", "Home tab");
+                break;
+            case R.id.navigation_setting:
+                Log.d("MinhLV", "Setting tab");
+                break;
+            case R.id.navigation_bolt:
+                Log.d("MinhLV", "Bolt tab");
+                break;
+            case R.id.navigation_user:
+                Log.d("MinhLV", "User tab");
+                break;
+        }
+        return true;
     }
 }
